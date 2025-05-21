494d67ab8100d0869f86fb8e026a2da290c0293f
alex <alexandr.paramon56@gmail.com>
2025-05-21 17:29:44 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Brackets_Analysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
