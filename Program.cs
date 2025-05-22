ff8584b24bf6562a545065b6304796187c0d086e
alex <alexandr.paramon56@gmail.com>
2025-05-22 16:34:48 +0500

    Feat (core): Added main algorithm and console interaction

--- Program.cs
+++ Program.cs
@@ -4,7 +4,97 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Hello! Enter your sequence of parentheses");
+                string? sequence = Console.ReadLine();
+                if (sequence == null)
+                {
+                    Console.WriteLine("Sequence cannot be null");
+                    continue;
+                }
+                bool result = CheckSequence(sequence);
+                Console.WriteLine($"Correctness of the sequence is: {result}");
+                Console.WriteLine();
+            }
+
+            bool CheckSequence(string sequence)
+            {
+                Dictionary<char, char> keyValuePairs = new Dictionary<char, char>()
+                {
+                    { '[' , ']' },
+                    { '{' , '}' },
+                    { '(' , ')' },
+                    { '<' , '>' },
+                };
+
+                Dictionary<KeyValuePair<char, char>, int> dictionary = new Dictionary<KeyValuePair<char, char>, int>();
+
+                foreach (var keyValuePair in keyValuePairs)
+                {
+                    dictionary.Add(keyValuePair, 0);
+                }
+
+                bool result = true;
+
+                int i = 0;
+                foreach (char item in sequence)
+                {
+                    i++;
+                    if (keyValuePairs.ContainsKey(item))
+                    {
+                        ++dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals( p.Key , item))];
+                    }
+                    else if (keyValuePairs.ContainsValue(item))
+                    {
+                        if ( --dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals( p.Value, item))] < 0 )
+                        {
+                            Console.WriteLine($"Open parenthese: {item}, Index: {i}");
+                            dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals(p.Value, item))] = 0;
+                            result = false;
+                        }
+                    }
+                }
+
+                foreach (var keyValuePair in keyValuePairs)
+                {
+                    dictionary[keyValuePair] =  0;
+                }
+
+                char[] arrayOfChars = sequence.ToCharArray();
+                Array.Reverse(arrayOfChars);
+                sequence = new string(arrayOfChars);
+                i = sequence.Length;
+                List<Tuple<char, int>> unclosed = new();
+                foreach (char item in sequence)
+                {
+                    i--;
+                    if (keyValuePairs.ContainsKey(item))
+                    {
+                        if ( --dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals(p.Key, item))] < 0)
+                        {
+                            unclosed.Add(Tuple.Create(item, i));
+                            dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals(p.Key, item))] = 0;
+                            result = false;
+                        }
+                    }
+                    else if (keyValuePairs.ContainsValue(item))
+                    {
+                        ++dictionary[keyValuePairs.FirstOrDefault(p => keyValuePairs.Comparer.Equals(p.Value, item))];
+                    }
+                }
+
+                if (unclosed.Count > 0) 
+                {
+                    var sorted = unclosed.OrderBy(item => item.Item2);
+                    foreach (var item in sorted)
+                    {
+                        Console.WriteLine($"Unclosed parenthese: {item.Item1}, Index: {item.Item2}");
+                    }
+                }
+
+                return result;
+            }
         }
     }
 }
