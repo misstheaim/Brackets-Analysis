10982cbee9a7f20d3301e2732dd8e4f991ab978a
alex <alexandr.paramon56@gmail.com>
2025-05-23 19:33:24 +0500

    feat (core): Added new aldorithm using Stack

--- Program.cs
+++ Program.cs
@@ -13,9 +13,81 @@
                     Console.WriteLine("Sequence cannot be null");
                     continue;
                 }
-                bool result = CheckSequence(sequence);
+                bool result = CheckSequence2(sequence);
                 Console.WriteLine($"Correctness of the sequence is: {result}");
                 Console.WriteLine();
+            }
+
+            bool CheckSequence2(string sequence)
+            {
+                bool result = true;
+
+                Dictionary<char, char> parenthesesPairs = new Dictionary<char, char>()
+                {
+                    { ']' , '[' },
+                    { '}' , '{' },
+                    { ')' , '(' },
+                    { '>' , '<' },
+                };
+
+                Stack<char> stack = new Stack<char>();
+
+                int i = 0;
+                foreach (char item in sequence)
+                {
+                    i++;
+                    if (parenthesesPairs.ContainsValue(item))
+                    {
+                        stack.Push(item);
+                    }
+                    else if (parenthesesPairs.ContainsKey(item))
+                    {
+                        char peek;
+                        if (stack.TryPeek(out peek) && peek == parenthesesPairs[item])
+                            stack.Pop();
+                        else
+                        {
+                            Console.WriteLine($"Open parenthese: {item}, Index: {i}");
+                            result = false;
+                        }
+                    }   
+                }
+
+                stack.Clear();
+                char[] arrayOfChars = sequence.ToCharArray();
+                Array.Reverse(arrayOfChars);
+                sequence = new string(arrayOfChars);
+                i = sequence.Length;
+                List<Tuple<char, int>> unclosed = new();
+
+                foreach (char item in sequence)
+                {
+                    if (parenthesesPairs.ContainsKey(item))
+                    {
+                        stack.Push(item);
+                    }
+                    else if (parenthesesPairs.ContainsValue(item))
+                    {
+                        char peek;
+                        if (stack.TryPeek(out peek) && peek == parenthesesPairs.FirstOrDefault(pp => pp.Value == item).Key)
+                            stack.Pop();
+                        else
+                            unclosed.Add(Tuple.Create(item, i));
+                    }
+                    i--;
+                }
+
+                if (unclosed.Count > 0)
+                {
+                    result = false;
+                    var sorted = unclosed.OrderBy(item => item.Item2);
+                    foreach (var item in sorted)
+                    {
+                        Console.WriteLine($"Unclosed parenthese: {item.Item1}, Index: {item.Item2}");
+                    }
+                }
+
+                return result;
             }
 
             bool CheckSequence(string sequence)
